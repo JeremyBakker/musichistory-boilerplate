c926afbe401684259ac1ae6070003f17fa8a2de5
Jeremy Bakker <jeremybakker@Jeremys-MacBook-Pro.local>
2017-01-24 13:06:46 -0600

    added xhr

--- javascripts/songs.js
+++ javascripts/songs.js
@@ -1,51 +1,42 @@
-// initialize an array
+// XHR 
+var dataRequest = new XMLHttpRequest();
+dataRequest.addEventListener("load", dataRequestComplete);
+dataRequest.addEventListener("error", dataRequestFailed);
 
 var songs = [];
-
-
-// add to the array using [songs.length] --- ([i] increases as songs are added)
-
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-
-
-// add songs to the beginning and end of the array
-
-songs.unshift("No Sleep Till Brooklyn > by Beastie Boys on the album Licensed to Ill");
-songs.push("Empire State of Mind > by Jay-Z on the album The Blueprint 3");
-
-
-// loop through the array and replace ">" with "-"
-
-for (var i = 0; i < songs.length; i++){
-	songs[i] = songs[i].replace(/>/g, "-");
-	console.log(songs[i]);
+function dataRequestComplete(event) {
+	console.log("The data transfer is complete.")
+	var data = JSON.parse(event.target.responseText);
+	songs = Object.values(data);
+	populateMusic();
 }
 
-
-/* loop through the array, replace special characters, and add the songs to the DOM in the 
-listContent main content area */
-
-for (var i = 0; i < songs.length; i++){
-	songs[i] = songs[i].replace(/[!@(*]/g, "");
-	console.log(songs[i]);
-	var song = songs[i].slice(0,songs[i].indexOf("by")-2);
-	var artist = songs[i].slice(songs[i].indexOf("by")+3, songs[i].indexOf(" on "));
-	var album = songs[i].slice(songs[i].indexOf("album")+6, songs[i].length+1);
-	document.getElementById("main").innerHTML += `<div class="musicEntry">
-		<p>${song}</p>
-		<ul>
-			<li>${artist}</li>
-			<li>${album}</li>
-			<li>Genre</li>
-		</ul>
-		<button class="delete">Delete</button>
-	</div>`;
+function dataRequestFailed() {
+	console.log("Oops, an error occurred while transferring the file.")
 }
-identifyButtons();
+
+dataRequest.open("GET", "songs.json");
+
+dataRequest.send();
+
+
+// add the songs to the DOM in the listContent main content area */
+
+function populateMusic(){
+	for (var i = 0; i < songs.length; i++){
+		console.log(i);
+		document.getElementById("main").innerHTML += `<div class="musicEntry">
+			<p>${songs[i].song}</p>
+			<ul>
+				<li>${songs[i].artist}</li>
+				<li>${songs[i].album}</li>
+				<li>Genre</li>
+			</ul>
+			<button class="delete">Delete</button>
+		</div>`;
+	}
+	identifyButtons();
+}
 
 
 // allow users to input data and add to the main section of the list view
@@ -106,23 +97,41 @@ function erase(event) {
 	this.parentNode.parentNode.removeChild(this.parentNode);
 }
 
+/////////////////////////////////////////////////////////////////////////////////
 
-// XHR 
-var dataRequest = new XMLHttpRequest();
-dataRequest.addEventListener("load", dataRequestComplete);
-dataRequest.addEventListener("error", dataRequestFailed);
+/************************************/
+/* Deprecated Code from previous 	*/
+/*   iterations of the assignment   */
+/************************************/
 
-function dataRequestComplete(event) {
-	console.log("The data transfer is complete.")
-	var data = JSON.parse(event.target.responseText); // converts from JSON to an array
-	console.log(data);
-	songs = data;
-}
 
-function dataRequestFailed() {
-	console.log("Oops, an error occurred while transferring the file.")
-}
+// // add to the array using [songs.length] --- ([i] increases as songs are added)
 
-dataRequest.open("GET", "songs.json");
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-dataRequest.send();
+
+// // add songs to the beginning and end of the array
+
+// songs.unshift("No Sleep Till Brooklyn > by Beastie Boys on the album Licensed to Ill");
+// songs.push("Empire State of Mind > by Jay-Z on the album The Blueprint 3");
+
+
+// // loop through the array and replace ">" with "-"
+
+// for (var i = 0; i < songs.length; i++){
+// 	songs[i] = songs[i].replace(/>/g, "-");
+// 	console.log(songs[i]);
+// }
+
+
+// // loop through the array, replace special characters, 
+
+// songs[i] = songs[i].replace(/[!@(*]/g, "");
+// console.log(songs[i]);
+// var song = songs[i].slice(0,songs[i].indexOf("by")-2);
+// var artist = songs[i].slice(songs[i].indexOf("by")+3, songs[i].indexOf(" on "));
+// var album = songs[i].slice(songs[i].indexOf("album")+6, songs[i].length+1);
